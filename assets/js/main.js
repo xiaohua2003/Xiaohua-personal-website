7f6e7ba641f8e6a64858ace70d216ea377fdec9f
xiaohua2003 <xcai3@ualberta.ca>
2021-06-27 22:16:33 -0600

    add resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -168,3 +168,26 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+//submit form
+
+var form = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+      alert('Thanks for your submission!')
+    //status.innerHTML = "Thanks for your submission!";
+    form.reset()
+  }).catch(error => {
+    alert("Oops! There was a problem submitting your form!") 
+  });
+}
+form.addEventListener("submit", handleSubmit)
